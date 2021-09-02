84c2e98bc08a6be684b6ae9308e31a65eae94846
Danilo Brauer <Danilo.Braeuer@web.de>
2021-09-02 12:52:41 +0200

    final commit before rebuild

---
+++ main.js
@@ -0,0 +1,7 @@
+const main__hidden;
+
+main__home = document.querySelector('#home');
+main__bookmarks = document.querySelector('#bookmarks');
+main__create = document.querySelector('#create');
+main__profile = document.querySelector('#profile');
+
